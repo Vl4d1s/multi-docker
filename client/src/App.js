16214470fae22fb7d138232a88d298ee64c94338
Vladis Markin <vladismarkin@gmail.com>
2021-10-18 01:53:37 +0300

    Trying somthing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import axios from "axios";
 
-function App() {
-  return <h1>Simple React App</h1>;
+class App extends React.Component {
+  state = {
+    message: "",
+  };
+
+  componentDidMount() {
+    this.fetchValues();
+  }
+
+  async fetchValues() {
+    const message = await axios.get("/api/");
+    this.setState({ message });
+  }
+
+  render() {
+    return (
+      <h1>{this.state.message ? this.state.message : "no server running"}</h1>
+    );
+  }
 }
-
 export default App;
